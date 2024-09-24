13e29d8a13f93908acb92097aa29514c20879524
Sean Li <cli049@uottawa.ca>
2024-09-24 15:04:35 -0400

    Added method to generate random room code

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+import string
+import random
+
+
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = "".join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+    return code
 
 
 # Create your models here.
