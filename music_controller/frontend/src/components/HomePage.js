117b83fd130b68dfc6c18eedab73c3d1d9b9c51b
Sean Li <cli049@uottawa.ca>
2024-09-29 15:17:52 -0400

    Updated HomePage.js

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -7,31 +7,30 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
-const renderHomePage = (props) => {
-    return (
-        <Grid container spacing={3}>
-            <Grid item xs={12} align="center">
-                <Typography variant="h3" compact="h3">
-                    House Party
-                </Typography>
-            </Grid>
-            <Grid item xs={12} align="center">
-                <Button disableElevation variant="contained" color="primary" to="/join" component={Link}>
-                    Join a Room
-                </Button>
-                <Button variant="contained" color="secondary" to="/create" component={Link}>
-                    Create a Room
-                </Button>
-            </Grid>
-        </Grid>
-    );
-};
-
 export default function HomePage(props) {
+    const RenderHomePage = () => {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" compact="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button disableElevation variant="contained" color="primary" to="/join" component={Link}>
+                        Join a Room
+                    </Button>
+                    <Button variant="contained" color="secondary" to="/create" component={Link}>
+                        Create a Room
+                    </Button>
+                </Grid>
+            </Grid>
+        );
+    };
     return (
         <Router>
             <Routes>
-                <Route exact path="/" element={renderHomePage} />
+                <Route exact path="/" element={<RenderHomePage />} />
                 <Route path="/join" element={<RoomJoinPage />} />
                 <Route path="/create" element={<CreateRoomPage />} />
                 <Route path="/room/:roomCode" element={<Room />} />
