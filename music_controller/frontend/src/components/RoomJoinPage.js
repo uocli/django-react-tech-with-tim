4b3060c8d710e60942cf14ff0c03543ec86b6022
Sean Li <cli049@uottawa.ca>
2024-09-29 14:30:43 -0400

    Updated RoomJoinPage.js

--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,13 +1,36 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { Link } from "@material-ui/core";
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
 
 export default class RoomJoinPage extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            roomCode: "",
+            error: "",
+        };
     }
 
     render() {
         return (
-            <div>Hello World from RoomJoinPage.js</div>
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                        Join a Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <TextField
+                        error={this.state.error}
+                        label="Code"
+                        placeholder="Enter a Room Code"
+                        value={this.state.roomCode}
+                        helperText={this.state.error}
+                        variant="outlined"
+                    />
+                </Grid>
+                <Button></Button>
+            </Grid>
         );
     }
 }
