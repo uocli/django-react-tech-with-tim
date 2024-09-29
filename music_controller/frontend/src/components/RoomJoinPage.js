0d14e0e54c0b2b2537fa966ce40f0af80a9b34ed
Sean Li <cli049@uottawa.ca>
2024-09-29 14:59:08 -0400

    Updated RoomJoinPage.js to be a function component which supports useNavigate

--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,56 +1,66 @@
-import React, { Component } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "@material-ui/core";
 import { TextField, Button, Grid, Typography } from "@material-ui/core";
 
-export default class RoomJoinPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            roomCode: "",
-            error: "",
+export default function RoomJoinPage(props) {
+    const navigate = useNavigate();
+    const [roomCode, setRoomCode] = React.useState("");
+    const [error, setError] = React.useState("");
+
+    const handleTextFieldChange = (event) => {
+        setRoomCode(event.target.value);
+    };
+
+    const roomButtonPressed = (event) => {
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                code: roomCode,
+            }),
         };
-        this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
-        this.roomButtonPressed = this.roomButtonPressed.bind(this);
-    }
+        fetch("/api/join-room", requestOptions)
+            .then((response) => {
+                if (response.ok) {
+                    navigate("/room/" + roomCode);
+                } else {
+                    setError("Room not found.");
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
-    handleTextFieldChange(event) {
-        this.setState({
-            roomCode: event.target.value,
-        });
-    }
-
-    roomButtonPressed(event) {}
-
-    render() {
-        return (
-            <Grid container spacing={1}>
-                <Grid item xs={12} align="center">
-                    <Typography variant="h4" component="h4">
-                        Join a Room
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <TextField
-                        error={this.state.error}
-                        label="Code"
-                        placeholder="Enter a Room Code"
-                        value={this.state.roomCode}
-                        helperText={this.state.error}
-                        variant="outlined"
-                        onChange={this.handleTextFieldChange}
-                    />
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <Button variant="contained" color="primary" onClick={this.roomButtonPressed}>
-                        Enter Room
-                    </Button>
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <Button variant="contained" color="secondary" to="/" component={Link}>
-                        Back
-                    </Button>
-                </Grid>
+    return (
+        <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+                <Typography variant="h4" component="h4">
+                    Join a Room
+                </Typography>
             </Grid>
-        );
-    }
+            <Grid item xs={12} align="center">
+                <TextField
+                    error={error}
+                    label="Code"
+                    placeholder="Enter a Room Code"
+                    value={roomCode}
+                    helperText={error}
+                    variant="outlined"
+                    onChange={handleTextFieldChange}
+                />
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button variant="contained" color="primary" onClick={roomButtonPressed}>
+                    Enter Room
+                </Button>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button variant="contained" color="secondary" to="/" component={Link}>
+                    Back
+                </Button>
+            </Grid>
+        </Grid>
+    );
 }
