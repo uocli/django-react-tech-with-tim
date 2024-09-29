c0cf1a91f99e501e6e44dfda3fd782be2126e118
Sean Li <cli049@uottawa.ca>
2024-09-29 12:18:23 -0400

    Updated CreateRoomPage.js to be a function component where useNavigate is supported

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
